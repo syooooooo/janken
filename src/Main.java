1df7f1bc449a4b0fae7a2f088bb3f7faaa9562a0
syougo <s.oouchida.sys23@morijyobi.ac.jp>
2024-07-12 12:30:04 +0900

    CPUの手をランダムにする処理を実装した close #15, close #16

--- src/Main.java
+++ src/Main.java
@@ -44,8 +44,12 @@ public class Main {
             System.exit(0);
             return;
         }
+
+        //comの手を生成
+        com.createRandomHand();
+
         //以降にテストコードを追加して検証を行う
-        System.out.println("テスト結果");
-        System.out.println(user.getHand().getHandName());
+        System.out.println("ランダムなジャンケンの手をCPUにセットする");
+        System.out.println(com.getHand().getHandName());
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,5 @@
 package src;
+import java.util.Random;
 
 public class Player {
     private String playerName;
@@ -6,6 +7,25 @@ public class Player {
 
     public Player(String playerName) {
         this.playerName = playerName;
+    }
+
+    /**
+     手の強弱を比較する。
+     返り値 -1 : 自身が引数で指定した手より弱い
+     　　　　0 : 自身が引数で指定した手と同じ
+     　　　　1 : 自身が引数で指定した手より強い
+     */
+    public void createRandomHand() {
+        Random random = new Random();
+        int randomValue = random.nextInt(3);
+
+        if(randomValue == 0) {
+            this.hand = new Gu();
+        } else if(randomValue == 1) {
+            this.hand = new Choki();
+        } else {
+            this.hand = new Pa();
+        }
     }
 
     public String getPlayerName() {
